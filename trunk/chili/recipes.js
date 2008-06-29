d68f3915c19a79653ff17daafcd5ce517fc64f1e
cappuccino.e.cornetto <cappuccino.e.cornetto@020cc513-074c-0410-bcc4-4ddb029c9b12>
2008-06-29 13:14:32 +0000

    from version 2.0 to 2.1

--- trunk/chili/recipes.js
+++ trunk/chili/recipes.js
@@ -23,12 +23,9 @@ ChiliBook.recipes[ "php.js" ] =
  * the trick is not perfect only if the html without php is broken
  * however, in such a case many highlighters get fooled but Chili does not
  * 
- * input: <p class="ex<?php echo $ex ? '"' : 'ample"' ?>>Hi!</p>
- *   (html without php [<p class="ex>Hi!</p>] is broken)
- * 
- * output of GeSHi 1.0.7.21: <span class="sy0">&lt;</span>p <span class="kw2">class</span><span class="sy0">=</span><span class="st0">"ex&lt;?php echo $ex ? '"</span><span class="st0">' : '</span>ample<span class="st0">"' ?&gt;&gt;Hi!&lt;/p&gt;</span>
- * 
- * output of Chili 2.0:      <span class="html__tag_start">&lt;p</span>&nbsp;class="ex<span class="php__start">&lt;?php</span>&nbsp;<span class="php__keyword">echo</span>&nbsp;<span class="php__keyword">$</span><span class="php__variable">ex</span>&nbsp;?&nbsp;<span class="php__string1">'"'</span>&nbsp;:&nbsp;<span class="php__string1">'ample"'</span>&nbsp;<span class="php__end">?&gt;</span><span class="html__tag_start">&gt;</span>Hi!<span class="html__tag_end">&lt;/p&gt;</span>
+ * ---
+ * this recipe has been adapted for working with Safari
+ * in fact, Safari cannot match more than 101236 characters with a lazy star
  * --------------------------------------------------------------------------*/
 {
 	  _name: "php"
@@ -40,7 +37,7 @@ ChiliBook.recipes[ "php.js" ] =
 				var placeholder = String.fromCharCode(0);
 				var blocks = [];
 				var that = this;
-				var no_php = all.replace( /<\?[\w\W]*?\?>|<\?[\w\W]*/g, function( block ) {
+				var no_php = all.replace( /<\?[^?]*\?+(?:[^>][^?]*\?+)*>|<\?[\w\W]*/g, function( block ) {
 					blocks.push( that.x( block, '/block/php' ) );
 					return placeholder;
 				} );
@@ -54,16 +51,16 @@ ChiliBook.recipes[ "php.js" ] =
 	}
 	, block: {
 		  php: {
-			  _match: /(<\?(?:php)?)([\w\W]*?)(\?>)|(<\?(?:php)?)([\w\W]*)/ 
-			, _replace: function( all, open, content, close, open2, content2 ) {
+			  _match: /(<\?(?:php\b)?)([^?]*\?+(?:[^>][^?]*\?+)*>)|(<\?(?:php\b)?)([\w\W]*)/
+			, _replace: function( all, open, content, open2, content2 ) {
 				if( open2 ) {
 					return "<span class='start'>" + this.x( open2 ) + "</span>"
 						+ this.x( content2, '/php' );
 				}
 				else {
 					return "<span class='start'>" + this.x( open ) + "</span>"
-						+ this.x( content, '/php' ) 
-						+ "<span class='end'>" + this.x( close ) + "</span>";
+						+ this.x( content.replace( /\?>$/, '' ), '/php' ) 
+						+ "<span class='end'>" + this.x( '?>' ) + "</span>";
 				}
 			}
 			, _style: {
